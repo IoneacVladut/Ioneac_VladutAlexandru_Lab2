a9532cc5607c973848e17714727496bc8665f106
vlad_ <vlad_@DESKTOP-9K38OAP>
2024-12-15 20:05:43 +0200

    Add project files.

---
+++ Data/Ioneac_VladutAlexandru_Lab2Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ioneac_VladutAlexandru_Lab2.Models;
+
+namespace Ioneac_VladutAlexandru_Lab2.Data
+{
+    public class Ioneac_VladutAlexandru_Lab2Context : DbContext
+    {
+        public Ioneac_VladutAlexandru_Lab2Context (DbContextOptions<Ioneac_VladutAlexandru_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Ioneac_VladutAlexandru_Lab2.Models.Book> Book { get; set; } = default!;
+        public DbSet<Ioneac_VladutAlexandru_Lab2.Models.Publisher> Publisher { get; set; } = default!;
+    }
+}
---
+++ Pages/Publishers/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Ioneac_VladutAlexandru_Lab2.Data;
+using Ioneac_VladutAlexandru_Lab2.Models;
+
+namespace Ioneac_VladutAlexandru_Lab2.Pages.Publishers
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Ioneac_VladutAlexandru_Lab2.Data.Ioneac_VladutAlexandru_Lab2Context _context;
+
+        public IndexModel(Ioneac_VladutAlexandru_Lab2.Data.Ioneac_VladutAlexandru_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Publisher> Publisher { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Publisher = await _context.Publisher.ToListAsync();
+        }
+    }
+}
